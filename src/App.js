9d3550976c359df137f9276b3d52314a89ff047e
ravi-kachhadiya-sailfin <95468178+ravi-kachhadiya-sailfin@users.noreply.github.com>
2022-08-10 18:50:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import FakeData from "./FakeData/Data.json";
 import "font-awesome/css/font-awesome.min.css";
 
 function App() {
-
   if (window.performance.navigation.type === window.performance.navigation.TYPE_RELOAD) {
     localStorage.setItem('kanbanData', JSON.stringify(FakeData));
   }
